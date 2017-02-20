205f88cad509bc3d7185f1bd81ccdfa194308376
bangtle <lethbang@yahoo.com>
2017-02-19 20:50:21 -0500

    5

--- R/multisite.R
+++ R/multisite.R
@@ -10,9 +10,10 @@
 #' @param randeff Existence of random site effects.
 #' @param alpha Probability of type I error.
 #' @seealso \code{\link{PowerUpR}}
-#' @examples 
+#' @examples
 #' powerMS(n=100,J=40,delta=0.2,rhoL2=0.3,numcovL1=0,R2L1=0,randeff=TRUE,alpha=0.05)
-#')
+#'
+
 powerMS <- function(n,J,delta,sigmad,rhoL2,numcovL1=0,R2L1=0,randeff=TRUE,alpha=0.05)
 {
   if ((n<=0)|!(n%%1==0)) stop("n must be a positive integer!")
@@ -25,7 +26,7 @@ powerMS <- function(n,J,delta,sigmad,rhoL2,numcovL1=0,R2L1=0,randeff=TRUE,alpha=
   else if ((numcovL1==0) & !(R2L1==0)) stop("numcovL1 = 0, so R2L1 must also be zero!")
   else if (!(randeff==TRUE)|!(randeff==T)&!(randeff==FALSE)|!(randeff==F)) stop("randeff can be either TRUE (or T) or FALSE (or F) only!")
   else if ((alpha<0)|(alpha>1)) stop("alpha must be between 0 and 1!")
-  
+
   else if ((numcovL1==0)&(randeff==FALSE)){ # model (5.1.1)
     num.df <- 1
     denom.df <- J-1
@@ -47,9 +48,9 @@ powerMS <- function(n,J,delta,sigmad,rhoL2,numcovL1=0,R2L1=0,randeff=TRUE,alpha=
     lambda <- J*delta^2/(4*(1-R2L2)*(1-rhoL2)/n)
   }
   else stop("Unrecognized model. Please specify a valid model!")
-  
+
   power <- 1-pf(qf(1-alpha,num.df,denom.df),num.df,denom.df,lambda)
-  
+
   result <- data.frame(n,J,delta,sigmad,rhoL2,numcovL1,R2L1,randeff,alpha,power)
   return(result)
 }
